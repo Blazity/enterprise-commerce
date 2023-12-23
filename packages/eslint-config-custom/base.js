5e433cd7b37da2d9e9315767b5e65cdc43d6d5b3
Bart Stefanski <contact@bstefanski.com>
2023-12-23 22:20:34 +0100

    chore: init monorepo

---
+++ packages/eslint-config-custom/base.js
@@ -0,0 +1,89 @@
+/* eslint-env es6 */
+/* eslint-disable no-console */
+
+const fs = require("fs")
+
+module.exports = {
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  extends: [
+    "next",
+    "prettier",
+    "react-app",
+    "react-app/jest",
+    "plugin:storybook/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+    ecmaVersion: "latest",
+  },
+  env: {
+    es6: true,
+  },
+  rules: {
+    "tailwindcss/no-custom-classname": "off",
+    "testing-library/prefer-screen-queries": "off",
+    "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+    "tailwindcss/classnames-order": "off",
+    "import/order": [
+      1,
+      {
+        groups: ["external", "builtin", "internal", "sibling", "parent", "index"],
+        pathGroups: [
+          ...getDirectoriesToSort().map((singleDir) => ({
+            pattern: `${singleDir}/**`,
+            group: "internal",
+          })),
+          {
+            pattern: "env",
+            group: "internal",
+          },
+          {
+            pattern: "theme",
+            group: "internal",
+          },
+          {
+            pattern: "public/**",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["internal"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+}
+
+function getDirectoriesToSort() {
+  const ignoredSortingDirectories = [".git", ".next", ".vscode", "node_modules"]
+  return getDirectories(process.cwd()).filter((f) => !ignoredSortingDirectories.includes(f))
+}
+
+function getDirectories(path) {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path + "/" + file).isDirectory()
+  })
+}
---
+++ renovate.json
@@ -0,0 +1,16 @@
+{
+  "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+  "extends": [
+    "config:base"
+  ],
+  "packageRules": [
+    {
+      "enabled": false,
+      "matchPackagePatterns": ["*"]
+    }
+  ],
+  "vulnerabilityAlerts": {
+    "enabled": true
+  },
+  "osvVulnerabilityAlerts": true
+}
