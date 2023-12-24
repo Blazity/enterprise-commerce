6e0032c951a1e2c36ac313275b260029f8b6f357
Bart Stefanski <contact@bstefanski.com>
2023-12-25 00:29:49 +0100

    fix: add playwright dependency & cache yarn in Github Actions

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from "@playwright/test"
+import { defineConfig } from "@playwright/test"
 
 /**
  * Read environment variables from file.
@@ -33,18 +33,10 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
+      name: "Google Chrome",
+      use: {
+        channel: "chrome",
+      },
     },
 
     /* Test against mobile viewports. */
