08faaa9a5564661416321cb4d8f8e1f6d5193f6f
Bart Stefanski <contact@bstefanski.com>
2024-02-13 22:25:10 +0100

    feat: add meilisearch host question to create-commerce cli

--- apps/create-commerce/src/components/CreateCommerceForm.tsx
+++ apps/create-commerce/src/components/CreateCommerceForm.tsx
@@ -6,11 +6,17 @@ import { Text, useFocusManager } from "ink"
 import { QuestionTextInput } from "./QuestionTextInput"
 import { QuestionSelectInput } from "./QuestionSelectInput"
 
-export type Step = "project-directory" | "package-manager" | "shopify-storefront-api-token" | "meilisearch-api-token"
+export type Step =
+  | "project-directory"
+  | "package-manager"
+  | "shopify-storefront-api-token"
+  | "meilisearch-api-token"
+  | "meilisearch-api-host"
 
 const projectDirectoryAtom = atom("")
 const shopifyStorefrontApiTokenAtom = atom("")
 const meilisearchApiTokenAtom = atom("")
+const meilisearchApiHostAtom = atom("")
 const packageManagerAtom = atom("")
 
 const formValuesAtom = atom((get) => {
@@ -18,11 +24,14 @@ const formValuesAtom = atom((get) => {
     "project-directory": get(projectDirectoryAtom),
     "shopify-storefront-api-token": get(shopifyStorefrontApiTokenAtom),
     "meilisearch-api-token": get(meilisearchApiTokenAtom),
+    "meilisearch-api-host": get(meilisearchApiHostAtom),
     "package-manager": get(packageManagerAtom),
   }
 })
 
-const completedStepsAtom = atom<Step[]>((get) => Object.entries(get(formValuesAtom)).map(([step, value]) => (value ? step : undefined)) as Step[])
+const completedStepsAtom = atom<Step[]>(
+  (get) => Object.entries(get(formValuesAtom)).map(([step, value]) => (value ? step : undefined)) as Step[]
+)
 
 export type CreateCommerceFormValues = Record<Step, string>
 
@@ -31,13 +40,15 @@ type CreateCommerceFormProps = {
   onFormSubmit: (formValues: CreateCommerceFormValues) => void
 }
 
+const NO_OF_STEPS = 5
+
 export function CreateCommerceForm({ onFormSubmit, defaultPackageManager }: CreateCommerceFormProps) {
   const [completedSteps] = useAtom(completedStepsAtom)
   const [formValues] = useAtom(formValuesAtom)
   const { disableFocus } = useFocusManager()
 
   useEffect(() => {
-    if (completedSteps.filter((step) => !isNil(step)).length === 4) {
+    if (completedSteps.filter((step) => !isNil(step)).length === NO_OF_STEPS) {
       onFormSubmit(formValues)
       disableFocus()
     }
@@ -51,7 +62,11 @@ export function CreateCommerceForm({ onFormSubmit, defaultPackageManager }: Crea
 
       {completedSteps.includes("shopify-storefront-api-token") ? <MeilisearchAPIToken /> : null}
 
-      {completedSteps.includes("meilisearch-api-token") ? <PackageManager defaultPackageManager={defaultPackageManager} /> : null}
+      {completedSteps.includes("meilisearch-api-token") ? <MeilisearchHost /> : null}
+
+      {completedSteps.includes("meilisearch-api-host") ? (
+        <PackageManager defaultPackageManager={defaultPackageManager} />
+      ) : null}
     </>
   )
 }
@@ -81,7 +96,8 @@ function ShopifyStorefrontAPIToken() {
     <QuestionTextInput
       id={stepName}
       nextFocusId="meilisearch-api-token"
-      question="Shopify Storefront API Token"
+      defaultValue="demo"
+      question="Shopify Storefront API Token (optional)"
       secretInput
       onEnter={(value) => {
         setShopifyStorefrontApiToken(value)
@@ -97,11 +113,30 @@ function MeilisearchAPIToken() {
   return (
     <QuestionTextInput
       id={stepName}
-      nextFocusId="package-manager"
-      question="Meilisearch API Token"
+      nextFocusId="meilisearch-api-host"
+      defaultValue="demo"
+      question="Meilisearch API Token (optional)"
       secretInput
       onEnter={(value) => {
         setMeilisearchApiToken(value)
+      }}
+    />
+  )
+}
+
+function MeilisearchHost() {
+  const stepName: Step = "meilisearch-api-host"
+  const [, setMeilisearchHost] = useAtom(meilisearchApiHostAtom)
+
+  return (
+    <QuestionTextInput
+      id={stepName}
+      nextFocusId="package-manager"
+      defaultValue="demo"
+      question="Meilisearch Host (optional)"
+      secretInput
+      onEnter={(value) => {
+        setMeilisearchHost(value)
       }}
     />
   )
