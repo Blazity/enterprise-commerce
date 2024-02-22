7adde2400ba2db8338cc38458e990cb6cd70cdb9
Bart Stefanski <contact@bstefanski.com>
2024-02-22 22:31:40 +0100

    chore: set searchProduct action revalidate time to 1 hour

--- apps/web/app/actions.ts
+++ apps/web/app/actions.ts
@@ -4,7 +4,7 @@ import { PlatformProduct } from "@enterprise-commerce/core/platform/types"
 import { meilisearch } from "client/meilisearch"
 import { unstable_cache } from "next/cache"
 
-export const searchProducts = unstable_cache(uncached_searchProducts, ["autocomplete-search"])
+export const searchProducts = unstable_cache(uncached_searchProducts, ["autocomplete-search"], { revalidate: 3600 })
 
 async function uncached_searchProducts(query: string, limit: number = 4): Promise<PlatformProduct[]> {
   const index = await meilisearch?.getIndex<PlatformProduct>("products")
