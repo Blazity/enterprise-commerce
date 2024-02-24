d0b5ffaa626e2da350d57d05c72cb6efbf91f5a6
Bart Stefanski <contact@bstefanski.com>
2024-02-25 00:11:49 +0100

    chore: remove avif from image optimization settings

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -17,7 +17,8 @@ const config = withPlugins([[withBundleAnalyzer({ enabled: process.env.ANALYZE =
   images: {
     deviceSizes: [320, 640, 750, 828, 1080, 1200],
     minimumCacheTTL: 31_556_926,
-    formats: ["image/avif", "image/webp"],
+    // formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
     remotePatterns: [
       {
         protocol: "https",
