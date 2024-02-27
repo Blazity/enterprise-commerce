79b9c12ff3fd948fcede1c4c4f04695aaa408a7b
Bart Stefanski <contact@bstefanski.com>
2024-02-27 14:56:18 +0100

    refactor: use GenericModal abstraction in all modals

--- apps/web/components/Modals/LoginModal.tsx
+++ apps/web/components/Modals/LoginModal.tsx
@@ -1,5 +1,6 @@
 import { Button } from "components/Button"
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "components/Dialog"
+import { DialogFooter } from "components/Dialog"
+import { GenericModal } from "components/GenericModal"
 import { Input } from "components/Input"
 import { Label } from "components/Label"
 import { useModalStore } from "stores/modalStore"
@@ -9,31 +10,26 @@ export function LoginModal() {
   const closeModal = useModalStore((s) => s.closeModal)
 
   return (
-    <Dialog open={!!modals["login"]} onOpenChange={() => closeModal("login")}>
-      <DialogContent className="max-w-[90%] bg-white sm:max-w-[425px] ">
-        <DialogHeader>
-          <DialogTitle>Login</DialogTitle>
-        </DialogHeader>
-        <div className="flex w-full flex-col items-center gap-6 px-0 py-4 md:px-2 md:pt-8">
-          <div className="flex w-full flex-col justify-center gap-4">
-            <Label htmlFor="name" className="text-md">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="flex w-full flex-col justify-center gap-4">
-            <Label htmlFor="username" className="text-md">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+    <GenericModal title="Login" open={!!modals["login"]} onOpenChange={() => closeModal("login")}>
+      <div className="flex w-full flex-col items-center gap-6 px-0 py-4 md:px-2 md:pt-8">
+        <div className="flex w-full flex-col justify-center gap-4">
+          <Label htmlFor="name" className="text-md">
+            Name
+          </Label>
+          <Input id="name" value="Pedro Duarte" className="col-span-3" />
         </div>
-        <DialogFooter>
-          <Button size="lg" className="hover:text-white" variant="secondary" isAnimated={false} type="submit">
-            Submit
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        <div className="flex w-full flex-col justify-center gap-4">
+          <Label htmlFor="username" className="text-md">
+            Username
+          </Label>
+          <Input id="username" value="@peduarte" className="col-span-3" />
+        </div>
+      </div>
+      <DialogFooter>
+        <Button size="lg" className="hover:text-white" variant="secondary" isAnimated={false} type="submit">
+          Submit
+        </Button>
+      </DialogFooter>
+    </GenericModal>
   )
 }
