71530d5877f479cc671f4aa598e4763c92d5a01e
Bart Stefanski <contact@bstefanski.com>
2024-03-14 22:48:24 +0100

    fix: add missing aria to product card links

--- apps/web/components/ProductCard/ProductCard.tsx
+++ apps/web/components/ProductCard/ProductCard.tsx
@@ -12,11 +12,12 @@ interface ProductCardProps extends PlatformProduct {
 export function ProductCard(props: ProductCardProps) {
   const variant = props.variants?.find(Boolean)?.price
   const href = `/products/${props.handle}`
+  const linkAria = `Visit product: ${props.title}`
 
   return (
     <div className={cn("group relative bg-neutral-100 p-4 md:bg-transparent md:p-0", props.className)}>
       <div className="relative flex min-h-[100px] items-center justify-center bg-neutral-100 md:min-h-[320px]">
-        <Link href={href} className="relative z-10">
+        <Link aria-label={linkAria} href={href} className="relative z-10">
           <Image
             alt={props.featuredImage?.altText || ""}
             className="size-[150px] select-none object-contain transition-transform group-hover:scale-105 md:size-[250px]"
@@ -31,7 +32,7 @@ export function ProductCard(props: ProductCardProps) {
         <QuickAdd variants={props.variants} />
       </div>
 
-      <Link href={href}>
+      <Link aria-label={linkAria} href={href}>
         <div className="mt-4 flex flex-col gap-0.5 text-slate-700">
           <div className="line-clamp-2 text-[13px] tracking-tight md:text-[19px]">{props.title}</div>
           {variant ? <p className="text-[13px] font-bold tracking-tight text-black md:text-[23px] md:font-normal">{variant.amount + " " + variant.currencyCode}</p> : null}
