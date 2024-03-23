d7050450624764e80b9c191443341169a9d9690f
Bart Stefanski <contact@bstefanski.com>
2024-03-23 22:06:08 +0100

    chore: bump root dependencies

--- apps/web/app/actions/cart.actions.ts
+++ apps/web/app/actions/cart.actions.ts
@@ -1,9 +1,9 @@
 "use server"
 
 import { storefrontClient } from "clients/storefrontClient"
-import { COOKIE_CART_ID, TAGS } from "constants/index"
 import { revalidateTag, unstable_cache } from "next/cache"
 import { cookies } from "next/headers"
+import { COOKIE_CART_ID, TAGS } from "constants/index"
 
 export const getCart = unstable_cache(async (cartId: string) => storefrontClient.getCart(cartId), [TAGS.CART], { revalidate: 60 * 15, tags: [TAGS.CART] })
 
