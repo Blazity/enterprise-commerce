5278477f2589ae9d1312b85f1949f021d4b05c4c
Bart Stefanski <contact@bstefanski.com>
2024-03-23 23:00:13 +0100

    chore: bump dev dependencies in web project

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -22,6 +22,16 @@ const config = withPlugins([[withVercelToolbar(), withBundleAnalyzer({ enabled: 
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "plus.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
         hostname: "picsum.photos",
         port: "",
       },
