ddb4bd1c3378c73c8faf88e4c0850c957ba41369
Bart Stefanski <contact@bstefanski.com>
2024-03-30 14:55:53 +0100

    fix: load DetailSection chunks only once

--- apps/web/views/Product/DetailsSection.tsx
+++ apps/web/views/Product/DetailsSection.tsx
@@ -5,12 +5,14 @@ import { useIntersectionObserver } from "@uidotdev/usehooks"
 import dynamic from "next/dynamic"
 import { getCombination, getOptionsFromUrl } from "utils/productOptionsUtils"
 import { AddToCartButtonSkeleton, FaqSectionSkeleton, VariantsSectionSkeleton } from "./PageSkeleton"
+import { useRef } from "react"
 
 const VariantsSection = dynamic(() => import("views/Product/VariantsSection").then((mod) => mod.VariantsSection), { loading: VariantsSectionSkeleton })
 const AddToCartButton = dynamic(() => import("views/Product/AddToCartButton").then((module) => module.AddToCartButton), { loading: AddToCartButtonSkeleton })
 const FaqSection = dynamic(() => import("views/Product/FaqSection").then((module) => module.FaqSection), { loading: FaqSectionSkeleton })
 
 export function DetailsSection({ product, slug }: { product: PlatformProduct; slug: string }) {
+  const hasLoaded = useRef(false)
   const [ref, entry] = useIntersectionObserver({
     threshold: 0,
     root: null,
@@ -21,13 +23,17 @@ export function DetailsSection({ product, slug }: { product: PlatformProduct; sl
   const hasOnlyOneVariant = product.variants.length <= 1
   const combination = getCombination(product, color, size)
 
-  const variantsSectionMarkup = entry?.isIntersecting ? <VariantsSection flatOptions={product.flatOptions} variants={product.variants} /> : null
+  if (!hasLoaded.current && entry?.isIntersecting) {
+    hasLoaded.current = true
+  }
+
+  const variantsSectionMarkup = hasLoaded.current ? <VariantsSection flatOptions={product.flatOptions} variants={product.variants} /> : null
 
   return (
     <div ref={ref} className="w-full">
       {hasOnlyOneVariant ? null : variantsSectionMarkup}
-      {entry?.isIntersecting ? <AddToCartButton className="my-8" combination={combination} /> : null}
-      {entry?.isIntersecting ? <FaqSection className="mt-12" /> : null}
+      {hasLoaded.current ? <AddToCartButton className="my-8" combination={combination} /> : null}
+      {hasLoaded.current ? <FaqSection className="mt-12" /> : null}
     </div>
   )
 }
