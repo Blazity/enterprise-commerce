eaad62625783f04a424fa9dfa5444c2b77742f01
Bart Stefanski <contact@bstefanski.com>
2024-04-02 03:51:34 +0200

    fix: disable button if out of stock

--- apps/web/views/Product/AddToCartButton.tsx
+++ apps/web/views/Product/AddToCartButton.tsx
@@ -1,16 +1,20 @@
 "use client"
 
 import { PlatformVariant } from "@enterprise-commerce/core/platform/types"
-import { addCartItem } from "app/actions/cart.actions"
+import { addCartItem, getItemAvailability } from "app/actions/cart.actions"
 import { Button } from "components/Button/Button"
-import { useEffect } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { useFormState, useFormStatus } from "react-dom"
 import { useCartStore } from "stores/cartStore"
 import { cn } from "utils/cn"
+import { getCookie } from "utils/getCookie"
 import { Combination } from "utils/productOptionsUtils"
+import { COOKIE_CART_ID } from "constants/index"
 
 export function AddToCartButton({ className, combination }: { className?: string; combination: Combination | PlatformVariant | undefined }) {
+  const [isPending, startTransition] = useTransition()
   const [state, formAction] = useFormState(addCartItem, { ok: false })
+  const [hasAnyAvailable, setHasAnyAvailable] = useState(false)
   const openCart = useCartStore((s) => s.openCart)
 
   const actionWithParams = formAction.bind(null, combination?.id)
@@ -19,14 +23,23 @@ export function AddToCartButton({ className, combination }: { className?: string
     state.ok && openCart()
   }, [openCart, state])
 
+  useEffect(() => {
+    startTransition(async () => {
+      const cartId = getCookie(COOKIE_CART_ID)
+      const itemAvailability = await getItemAvailability(cartId, combination?.id)
+
+      itemAvailability && setHasAnyAvailable(itemAvailability.inCartQuantity < itemAvailability.inStockQuantity)
+    })
+  }, [combination?.id, state])
+
   return (
     <form className={className} action={actionWithParams}>
-      <Submit>{combination?.availableForSale ? "Add to Cart" : "Out Of Stock"}</Submit>
+      <Submit disabled={!hasAnyAvailable || isPending}>{combination?.availableForSale ? "Add to Cart" : "Out Of Stock"}</Submit>
     </form>
   )
 }
 
-function Submit({ children }) {
+function Submit({ children, disabled }) {
   const { pending } = useFormStatus()
 
   return (
@@ -38,7 +51,7 @@ function Submit({ children }) {
       isAnimated={false}
       className={cn("relative w-fit rounded-xl transition-transform hover:scale-105 hover:text-white")}
       isLoading={pending}
-      disabled={pending}
+      disabled={pending || disabled}
     >
       {children}
     </Button>
