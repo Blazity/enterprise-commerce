cb5d145c6df81ea53994e33e071c650c4987ce44
Bart Stefanski <contact@bstefanski.com>
2024-04-04 19:45:05 +0200

    fix: add aria label to mobile search

--- apps/web/components/NavigationBar/SearchButton.tsx
+++ apps/web/components/NavigationBar/SearchButton.tsx
@@ -9,7 +9,7 @@ export function SearchButton() {
   const openModal = useModalStore((s) => s.openModal)
 
   return (
-    <Button variant="ghost" className="ml-3 flex items-center justify-center p-1" onClick={() => openModal("search")}>
+    <Button aria-label="Launch search modal" variant="ghost" className="ml-3 flex items-center justify-center p-1" onClick={() => openModal("search")}>
       <ThinSearchIcon className="size-6" />
     </Button>
   )
