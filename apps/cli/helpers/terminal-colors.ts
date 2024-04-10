c3120cc294e2f4166914dbc14ded56f910878ead
Bart Stefanski <contact@bstefanski.com>
2024-04-10 22:57:51 +0200

    chore: remove cli & docs apps from the project

--- apps/cli/helpers/terminal-colors.ts
+++
@@ -1,4 +0,0 @@
-export const terminalColors = {
-  textOnBrightBackground: "#000",
-  blazity: "#ff6a00",
-}
--- apps/cli/prettier.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  trailingComma: "es5",
-  tabWidth: 2,
-  printWidth: 180,
-  semi: false,
-};
