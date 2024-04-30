278a7ad833c854332321603d7116e77cc7607449
ddaoxuan <dawid.daoxuan.uk@gmail.com>
2024-04-30 21:29:22 +0200

    chore: ui tweaks

--- apps/web/components/ProductCard/QuickAdd.tsx
+++ apps/web/components/ProductCard/QuickAdd.tsx
@@ -14,7 +14,7 @@ export function QuickAdd({ product, variants }: QuickAddProps) {
   const combinationsMarkup = combinations?.map((combination) => <QuickAddButton product={product} key={combination.id} combination={combination} withPrice />)
 
   return (
-    <div className="absolute inset-x-0 bottom-0 z-50 hidden h-0 w-full overflow-hidden bg-white transition-all group-hover:h-[90px] group-hover:border-t group-hover:border-black lg:block">
+    <div className="absolute inset-x-0 bottom-0 z-30 hidden h-0 w-full overflow-hidden bg-white transition-all group-hover:h-[90px] group-hover:border-t group-hover:border-black lg:block">
       <div className="p-2">
         <p className="mb-2 text-center text-[14px] font-bold uppercase tracking-wide">Quick add</p>
         <div className="no-scrollbar flex justify-start gap-1 overflow-x-auto">
