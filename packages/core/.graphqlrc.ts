6aea67efd9cdcb660abbcc6889607c003cc6307e
Dawid Dao Xuan <dawid.daoxuan.uk@gmail.com>
2024-07-17 02:23:41 +0200

    chore(*)!: deps cleanup (#48)

--- packages/core/.graphqlrc.ts
+++ packages/core/.graphqlrc.ts
@@ -2,19 +2,19 @@ import { ApiType, shopifyApiProject } from "@shopify/api-codegen-preset"
 
 export default {
   schema: ["https://shopify.dev/storefront-graphql-direct-proxy/2024-01", "https://shopify.dev/admin-graphql-direct-proxy/2024-01"],
-  documents: ["./packages/core/**/*.{js,ts,jsx,tsx}"],
+  documents: ["./**/*.{js,ts,jsx,tsx}"],
   projects: {
     default: shopifyApiProject({
       apiType: ApiType.Storefront,
       apiVersion: "2024-01",
-      documents: ["./packages/core/**/*.storefront.{js,ts,jsx,tsx}", "./packages/core/**/fragments/*.{js,ts,jsx,tsx}"],
-      outputDir: "./packages/core/platform/shopify/types",
+      documents: ["./platform/shopify/**/*.storefront.{js,ts,jsx,tsx}", "./platform/shopify/**/fragments/*.{js,ts,jsx,tsx}"],
+      outputDir: "./platform/shopify/types",
     }),
     admin: shopifyApiProject({
       apiType: ApiType.Admin,
       apiVersion: "2024-01",
-      documents: ["./packages/core/**/*.admin.{js,ts,jsx,tsx}"],
-      outputDir: "./packages/core/platform/shopify/types/admin",
+      documents: ["./platform/shopify/**/*.admin.{js,ts,jsx,tsx}"],
+      outputDir: "./platform/shopify/types/admin",
     }),
   },
 }
