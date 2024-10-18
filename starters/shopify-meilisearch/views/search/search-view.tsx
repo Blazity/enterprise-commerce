38794c9c6e9c9b7cdae204e316e2a3fff0e1731e
kasin-it <kacperosinilo@gmail.com>
2024-10-18 12:24:45 +0200

    fix: lint

--- starters/shopify-meilisearch/views/search/search-view.tsx
+++ starters/shopify-meilisearch/views/search/search-view.tsx
@@ -66,7 +66,7 @@ export async function SearchView({ searchParams, disabledFacets, collection }: S
   return (
     <div className="mx-auto w-full max-w-[1920px] p-4 md:px-12 md:pb-24 md:pt-4">
       <div className="sticky top-[77px] z-40 flex items-center justify-between bg-white py-4 md:top-[84px] md:-mx-12 md:px-12">
-        <h1 className="flex items-center text-3xl gap-1 font-semibold tracking-tight lg:text-4xl">
+        <h1 className="flex items-center gap-1 text-3xl font-semibold tracking-tight lg:text-4xl">
           {makePageTitle(collection, q)}
           <span className="hidden lg:block">({totalHits})</span>
         </h1>
