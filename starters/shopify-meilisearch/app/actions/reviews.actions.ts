be94644580c51769d3da81d67bf281c9af84d3fb
Dawid Dao Xuan <dawid.daoxuan.uk@gmail.com>
2024-10-31 21:46:15 +0100

    refactor: move reviews lib (#87)

--- starters/shopify-meilisearch/app/actions/reviews.actions.ts
+++ starters/shopify-meilisearch/app/actions/reviews.actions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { reviewsClient } from "clients/reviews"
+import { createProductReview } from "lib/reviews"
 import type { ProductReviewBody } from "lib/reviews/types"
 
 import { headers } from "next/headers"
@@ -8,7 +8,7 @@ import { headers } from "next/headers"
 export const submitReview = async (payload: Omit<ProductReviewBody, "ip_addr">) => {
   try {
     const ipAddress = headers().get("x-forwarded-for") || null
-    await reviewsClient.createProductReview({ ...payload, ip_addr: ipAddress })
+    await createProductReview({ ...payload, ip_addr: ipAddress })
   } catch (err) {
     throw new Error(err as string)
   }
