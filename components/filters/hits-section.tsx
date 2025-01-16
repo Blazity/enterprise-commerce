583b49285b02d435e8974cdd845a9f88da91baff
Jakub Ziemba <ziemba.jak@gmail.com>
2025-01-16 22:32:03 +0100

    wip: add basic empty state for no queries

--- components/filters/hits-section.tsx
+++ components/filters/hits-section.tsx
@@ -4,6 +4,8 @@ import { ProductCard } from "components/product-card"
 import * as m from "motion/react-m"
 import { LazyMotion, domAnimation } from "motion/react"
 import type { CommerceProduct } from "types"
+import { usePathname } from "next/navigation"
+import { cn } from "utils/cn"
 
 interface HitsSectionProps {
   hits: CommerceProduct[]
@@ -11,13 +13,20 @@ interface HitsSectionProps {
 }
 
 export function HitsSection({ hits, basePath }: HitsSectionProps) {
+  const pathname = usePathname()
+  const isAiPath = pathname.startsWith("/ai")
+
   if (!hits.length) {
-    return <p>No results for this query</p>
+    return (
+      <div className="mt-2 flex h-[400px] w-full flex-col items-center justify-center rounded-xl bg-gray-50">
+        <p className="text-lg font-medium text-gray-500">No results for this query</p>
+      </div>
+    )
   }
 
   return (
     <LazyMotion features={domAnimation}>
-      <div className="-px-4 grid w-full grid-cols-2 items-start gap-1 gap-y-8 sm:grid-cols-3 sm:gap-6 xl:grid-cols-3">
+      <div className={cn("-px-4 grid w-full grid-cols-2 items-start gap-1 gap-y-8 sm:grid-cols-3 sm:gap-6 xl:grid-cols-3", isAiPath && "sm:grid-cols-2")}>
         {hits.map((singleResult, idx) => (
           <m.div key={singleResult.id} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.2, delay: 0.02 * idx }}>
             <ProductCard priority={[0, 1].includes(idx)} {...singleResult} href={basePath ? `/${basePath}/product/${singleResult.handle}` : undefined} />
