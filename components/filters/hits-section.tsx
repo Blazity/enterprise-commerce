1739a936823dcd5f621d2534dc295bb4c9ba161a
Jakub Ziemba <ziemba.jak@gmail.com>
2025-01-17 03:59:45 +0100

    fix: grid gap

--- components/filters/hits-section.tsx
+++ components/filters/hits-section.tsx
@@ -26,7 +26,7 @@ export function HitsSection({ hits, basePath }: HitsSectionProps) {
 
   return (
     <LazyMotion features={domAnimation}>
-      <div className={cn("-px-4 grid w-full auto-rows-fr grid-cols-2 items-start gap-1 gap-y-8 sm:grid-cols-3 sm:gap-6 xl:grid-cols-3", isAiPath && "sm:grid-cols-2")}>
+      <div className={cn("-px-4 grid w-full auto-rows-fr grid-cols-2 items-start gap-2 gap-y-4 sm:grid-cols-3 sm:gap-4 xl:grid-cols-3", isAiPath && "sm:grid-cols-2")}>
         {hits.map((singleResult, idx) => (
           <m.div key={singleResult.id} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.2, delay: 0.02 * idx }} className="h-full">
             <ProductCard priority={[0, 1].includes(idx)} {...singleResult} href={basePath ? `/${basePath}/product/${singleResult.handle}` : undefined} />
