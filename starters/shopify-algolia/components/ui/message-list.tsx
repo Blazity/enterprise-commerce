74330c42fb56fefc7e5f57a0c5431e00ebd36937
ddaoxuan <dawid.daoxuan.uk@gmail.com>
2025-03-07 00:38:56 +0100

    chore: fix autoscroll when new messages pop up

--- starters/shopify-algolia/components/ui/message-list.tsx
+++ starters/shopify-algolia/components/ui/message-list.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from "react";
 import { ChatMessage, ChatMessageProps, Message } from "./chat-message"
 import { TypingIndicator } from "./typing-indicator"
 
@@ -11,8 +12,16 @@ interface MessageListProps {
 }
 
 export function MessageList({ messages, showTimeStamps = true, isTyping = false, messageOptions }: MessageListProps) {
+  const messageListRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
-    <div className="space-y-4 overflow-visible py-8">
+    <div ref={messageListRef} className="space-y-4 overflow-auto py-8">
       {messages.map((message, index) => {
         const additionalOptions = typeof messageOptions === "function" ? messageOptions(message) : messageOptions
 
