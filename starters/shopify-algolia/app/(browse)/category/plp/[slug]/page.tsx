b219e4d247ba8f5adb9a4f156c6ee8eea0c01c1f
Igor Klepacki <neg4n@icloud.com>
2025-06-26 21:55:53 +0200

    feat(algolia): add client side search to vendors and fix facet params

--- starters/shopify-algolia/app/(browse)/category/plp/[slug]/page.tsx
+++ starters/shopify-algolia/app/(browse)/category/plp/[slug]/page.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next"
+import { redirect } from "next/navigation"
 import { SearchParamsType } from "types"
 import { CategoryPLPView } from "components/category/category-plp-view"
 import { isDemoMode } from "utils/demo-utils"
-import { getCategories } from "lib/algolia"
+import { getCategories, getCollection } from "lib/algolia"
 
 export const runtime = "nodejs"
 export const revalidate = 86400
-export const dynamic = "force-static"
+// Allow dynamic rendering for query parameters
+export const dynamic = "force-dynamic"
 
 interface ProductListingPageProps {
   searchParams: Promise<SearchParamsType>
@@ -35,5 +37,6 @@ export async function generateStaticParams() {
 export default async function ProductListingPage(props: ProductListingPageProps) {
   const params = await props.params
   const searchParams = await props.searchParams
+  
   return <CategoryPLPView params={params} searchParams={searchParams} />
 }
