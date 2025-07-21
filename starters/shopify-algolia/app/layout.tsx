acb8d33f2a0c54e1af1e3b6e1e8fd31cf1919a07
Igor Klepacki <neg4n@icloud.com>
2025-07-21 04:41:18 +0200

    fix(algolia): unify the metadata

--- starters/shopify-algolia/app/layout.tsx
+++ starters/shopify-algolia/app/layout.tsx
@@ -1,13 +1,10 @@
 import { env } from "env.mjs"
 import { Metadata } from "next"
+import { sharedMetadata} from "./shared-metadata"
 
 export const metadata: Metadata = {
-  metadataBase: new URL(env.LIVE_URL || "https://commerce.blazity.com"),
-  title: {
-    template: "%s | Enterprise Commerce",
-    default: "Enterprise Commerce",
-  },
-  description: "AI-FIRST NEXT.JS STOREFRONT FOR COMPOSABLE COMMERCE",
+  metadataBase: sharedMetadata.metadataBase,
+  description: sharedMetadata.openGraph.description,
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
