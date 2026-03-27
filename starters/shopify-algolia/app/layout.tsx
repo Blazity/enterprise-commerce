58bab509028d03db9123370fde6327a4041b57bf
Kacper <89151689+kasin-it@users.noreply.github.com>
2026-03-27 10:12:32 +0100

    feat: disallow indexing (#127)

--- starters/shopify-algolia/app/layout.tsx
+++ starters/shopify-algolia/app/layout.tsx
@@ -5,6 +5,10 @@ import { sharedMetadata} from "./shared-metadata"
 export const metadata: Metadata = {
   metadataBase: sharedMetadata.metadataBase,
   description: sharedMetadata.openGraph.description,
+  robots: {
+    index: false,
+    follow: false,
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
