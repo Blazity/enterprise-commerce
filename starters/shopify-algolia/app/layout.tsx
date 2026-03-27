4bc67922da316ea09608d30504551270b3815a5a
Kacper <89151689+kasin-it@users.noreply.github.com>
2026-03-27 10:27:36 +0100

    Feat/disallow indexing (#128)
    * feat: disallow indexing
    
    * feat: modify robots.txt

--- starters/shopify-algolia/app/layout.tsx
+++ starters/shopify-algolia/app/layout.tsx
@@ -1,6 +1,6 @@
 import { env } from "env.mjs"
 import { Metadata } from "next"
-import { sharedMetadata} from "./shared-metadata"
+import { sharedMetadata } from "./shared-metadata"
 
 export const metadata: Metadata = {
   metadataBase: sharedMetadata.metadataBase,
