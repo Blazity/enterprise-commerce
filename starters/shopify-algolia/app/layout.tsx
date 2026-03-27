7f1e966178a66302126dd3c49472a4f0d03782a8
kasin-it <kacperosinilo@gmail.com>
2026-03-27 10:22:51 +0100

    feat: modify robots.txt

--- starters/shopify-algolia/app/layout.tsx
+++ starters/shopify-algolia/app/layout.tsx
@@ -1,14 +1,10 @@
 import { env } from "env.mjs"
 import { Metadata } from "next"
-import { sharedMetadata} from "./shared-metadata"
+import { sharedMetadata } from "./shared-metadata"
 
 export const metadata: Metadata = {
   metadataBase: sharedMetadata.metadataBase,
   description: sharedMetadata.openGraph.description,
-  robots: {
-    index: false,
-    follow: false,
-  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
